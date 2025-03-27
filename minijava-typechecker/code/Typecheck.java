13f0a856ce34e0695222ed31a3ff090eec3fbbd6
Will Schmitz <Willeujeans@gmail.com>
2025-03-27 15:24:40 -0600

    Comments out unwritten method calls

--- minijava-typechecker/code/Typecheck.java
+++ minijava-typechecker/code/Typecheck.java
@@ -14,32 +14,36 @@ public class Typecheck {
 
 	    // Pretty-print the tree. PPrinter inherits from
 	    // GJDepthFirst<R,A>. R=Void, A=String.
+		
 	    PPrinter<Void,String> pp = new PPrinter<Void,String>();
 	    root.accept(pp, "");
 
 	    // Build the symbol table. Top-down visitor, inherits from
 	    // GJDepthFirst<R,A>. R=Void, A=Integer.
-	    SymTableVis<Void, Integer> pv =
-	    	new SymTableVis<Void,Integer>();
-	    root.accept(pv, 0);
-	    HashMap<String, String> symt = pv.symt;
+
+	    // SymTableVis<Void, Integer> pv =
+	    // 	new SymTableVis<Void,Integer>();
+	    // root.accept(pv, 0);
+	    // HashMap<String, String> symt = pv.symt;
 
 	    // Do type checking. Bottom-up visitor, also inherits from
 	    // GJDepthFirst. Visit functions return MyTpe (=R), and
 	    // take a symbol table (HashMap<String,String>) as
 	    // argument (=A). You may implement things differently of
 	    // course!
-	    TypeCheckSimp ts = new TypeCheckSimp();
-	    MyType res = root.accept(ts, symt);
+
+	    // TypeCheckSimp ts = new TypeCheckSimp();
+	    // MyType res = root.accept(ts, symt);
 
 	    // Ugly code not to be inspired from: "my" way of storing
 	    // type info / typecheck property: if some of my internal
 	    // structure is empty, then things don't typecheck for
 	    // me. This is specific to my own implementation.
-	    if (res != null && res.type_array.size() > 0)
-	    	System.out.println("Code typechecks");
-	    else
-	    	System.out.println("Type error");
+
+	    // if (res != null && res.type_array.size() > 0)
+	    // 	System.out.println("Code typechecks");
+	    // else
+	    // 	System.out.println("Type error");
 	}
 	catch (ParseException e) {
 	    System.out.println(e.toString());
