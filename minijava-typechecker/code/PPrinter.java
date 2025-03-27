4418bb3e6f69fecdffee7b75c7796fb837ad961e
Will Schmitz <Willeujeans@gmail.com>
2025-03-27 15:30:30 -0600

    Add visit(Goal) functionality

--- minijava-typechecker/code/PPrinter.java
+++ minijava-typechecker/code/PPrinter.java
@@ -9,4 +9,26 @@ public class PPrinter<R, A> extends GJDepthFirst<Void, String> {
         System.out.println(output.toString());
         System.out.println("{ PPrinter } ---\n");
     }
+
+    /**
+    * Goal variables:
+    * f0 -> MainClass()
+    * f1 -> ( TypeDeclaration() )*
+    * f2 -> <EOF>
+    */
+    @Override
+    public Void visit(Goal n, String indent) {
+        output.append(indent);
+        output.append(indent).append("[ Goal ] (Root)\n");
+    
+        // f0 -> MainClass()
+        output.append(indent).append("[ MainClass ]:\n");
+        n.f0.accept(this, indent + ">");
+    
+        // f1 -> ( TypeDeclaration() )*
+        output.append(indent).append("[ TypeDeclarations ]:\n");
+        n.f1.accept(this, indent + ">");
+
+        return null;
+    }
 }
