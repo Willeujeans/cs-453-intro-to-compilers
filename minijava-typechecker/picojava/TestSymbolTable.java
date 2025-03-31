20d114fed8b22dc541c5443c4bd14f0a1c05e748
Will Schmitz <Willeujeans@gmail.com>
2025-03-31 13:17:12 -0600

    Add insert validation test case

--- minijava-typechecker/picojava/TestSymbolTable.java
+++ minijava-typechecker/picojava/TestSymbolTable.java
@@ -1,8 +1,25 @@
 package picojava;
 
+import picojava.MyType;
+import picojava.SymbolTable;
+
 public class TestSymbolTable {
     public TestSymbolTable(){
+        testInsertInvalidIdentifier();
         testInsert();
+    }
+
+    // Testing insert methods
+    public static void testInsertInvalidIdentifier() {
+        String output = "[TestSymbolTable] TEST: Insert Invalid Identifier";
+        SymbolTable testingSymbolTable = new SymbolTable();
+        SymbolData symbolData = new SymbolData(new MyType("String"), 0, 0, 0, 0, 0);
+        try {
+            testingSymbolTable.insert(null, symbolData);
+        }catch(IllegalArgumentException exception){
+            output += " ✅";
+            System.out.println(output);
+        }
     }
 
     public static void testInsert() {
