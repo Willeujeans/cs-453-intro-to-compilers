cf7c06f41df35881bb527ec700240069ca25719f
Will Schmitz <Willeujeans@gmail.com>
2025-03-30 18:21:32 -0600

    Add skeleton code for Symbol table methods

--- minijava-typechecker/picojava/SymbolTable.java
+++ minijava-typechecker/picojava/SymbolTable.java
@@ -14,4 +14,44 @@ public class SymbolTable {
         public final int address = 0;
     }
 
+    public SymbolTable() {
+        scopes = new LinkedList<>();
+        enterScope();
+    }
+
+    public LinkedList<HashMap<String, SymbolEntry>> getScopes(){
+        if(this.scopes != null && !this.scopes.isEmpty()){
+            return this.scopes;
+        }else{
+            System.err.println("SymbolTable: Trying to get an empty LinkedList");
+            return null;
+        }
+    }
+    
+    public void insert(String identifier, SymbolEntry entry){
+        // Adds symbols to the symbol table
+    }
+
+    public SymbolEntry lookup(String identifier){
+        // Lookup will find the symbolEntry inside the inner most scope using the identifier and symbol table
+        return null;
+    }
+
+    public boolean set(String identifier, int lineUsed){
+        // Updates a symbol table entry, sucess will return true, failure will return false
+        return false;
+    }
+
+    public void enterScope() {
+        // Enter a new scope
+    }
+
+    public void exitScope() {
+        // Exit current scope
+    }
+
+    public void reset(){
+        // Reset all non-global scopes
+    }
+
 }
